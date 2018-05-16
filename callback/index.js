46ad29bfcec0dcaf4ca17e639377b48101712053
Kyle Combs <combskyle@gmail.com>
2018-05-16 15:14:30 -0400

    added songKick API Functionality

--- callback/index.js
+++ callback/index.js
@@ -1,4 +1,7 @@
-//Autocomplete functionality for search box
+//------------------------------------------------------------
+//GoogleMaps API
+//------------------------------------------------------------
+
 function activatePlacesSearch(){
     const input = document.getElementById('search-term');
     const options = {
@@ -7,14 +10,81 @@ function activatePlacesSearch(){
     let autocomplete = new google.maps.places.Autocomplete(input, options);
 };
 
+//------------------------------------------------------------
+//Songkick API
+//------------------------------------------------------------
+
+let songKickDate;
+
+function getMetroAreaIDFromSongKick(city) {
+	settings = {
+		url: 'http://api.songkick.com/api/3.0/search/locations.json',
+		data: {
+			query: city,
+			apikey: 'TGXBOdEJdH8k5A5w'
+		},
+		dataType: 'json',
+		type: 'GET',
+		success: function(data) {
+			const metroAreaID = data.resultsPage.results.location[0].metroArea.id;
+			console.log(metroAreaID);
+			getConcertListingsFromSongKick(metroAreaID);
+		}
+	}
+	$.ajax(settings);
+}
+
+function getConcertListingsFromSongKick(id) {
+	formatDateForSongKick();
+	settings = {
+		url: `http://api.songkick.com/api/3.0/metro_areas/${id}/calendar.json`,
+		data: {
+			apikey: 'TGXBOdEJdH8k5A5w',
+			min_date: songKickDate,
+			max_date: songKickDate,
+		},
+		dataType: 'json',
+		type: 'GET',
+		success: function(data) {
+			console.log(data);
+			let concerts = data.resultsPage.results.event;
+			console.log(concerts);
+			createArtistList(concerts);
+			displayConcertListings(concerts);
+		}
+	}
+	$.ajax(settings);
+}
+
+function formatDateForSongKick() {
+	let today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; 
+	var yyyy = today.getFullYear();
+	if(dd<10) {
+    	dd='0'+dd;
+	} 
+	if(mm<10) {
+    	mm='0'+mm;
+	}
+	songKickDate = `${yyyy}-${mm}-${dd}`
+}
+
+
+
+
+
+
 //Build Concert Listings
 function renderResult(result) {
 	return `
 		<div class='concertListing'>
-			<img src="${result.artistImage}" class="artistImg" alt="${result.artist}" width='74' height='74'>
-			<span class='artistName'>${result.artist}</span><br>
-			${result.venue}, ${result.city}<br>
-			<a href="${result.ticketLink}">
+			<img src="https://images.sk-static.com/images/media/profile_images/artists/${result.performance[0].artist.id}/large_avatar" class="artistImg" alt="${result.performance[0].artist.displayName}" width='74' height='74'>
+			<a href="https://www.songkick.com/artists/${result.performance[0].artist.id}"
+				<span class='artistName'>${result.performance[0].artist.displayName}</span>
+			</a><br>
+			${result.venue.displayName}, ${result.venue.metroArea.displayName}, ${result.venue.metroArea.state.displayName}<br>
+			<a href="http://www.songkick.com/concerts/${result.id}">
 				<span class=button buy-tickets>Buy Tickets</span>
 			</a>
 		</div>`
@@ -30,8 +100,9 @@ function displayConcertListings(data) {
 //Create array of artitsts from concert listings
 let artistList = []
 function createArtistList(data) {
-	artistList = data.map(a => a.artist);
+	artistList = data.map(a => a.performance[0].artist.displayName);
 	console.log(artistList);
+	spotifyAppInitiate();
 }
 
 //------------------------------------------------------------
@@ -47,7 +118,7 @@ function getAccessToken() {
   return getParameterByName('access_token');
 }
 
-const accessToken = 'BQClGcducTRYlNdK345oPYq1yDXKVOq_AnNZbRC_GxB1JBRTozvimVFuOdJsEuryJFOOiXEBXEThqM0wLLMU5qsMxre9vqm5cSGBLsZIMXHb-OoNiHW1zgQtK-S2zDuUoM7Yrf_ekFyx_CCivx9BWDtzljIvsHMRzAT4EHr8NE-Io5vxdnro9gTeI_eyO4fkF-6V34RYksRWEGFwXinU724fni35';
+const accessToken = 'BQBkU6vinm6SioHamdB6vtFwUgmk6IaYbdaHKiYzBzqGXIERrVHeIGUWfS0xY9FUABQacCrsMkvz4JsAV1aBnRPN-gqpR9TR2F4t4xxU3MR-BbDHFDsWloU3lltbpIsQ8b6D71qyMlRrRCavgCf2Id2iydiwSxciY4w143d5toC3l-J5Eh1idPwYTAuLoKKO_4qBmLHJY1bLWnMQEbaJpFhTWIhh';
 
 //Get artists from spotify
 const getArtistsFromSpotify = (artist) => {
@@ -92,23 +163,7 @@ const getArtistsTracksFromSpotify = (id) => {
 }
 
 
-function todaysDate() {
-	let day = new Date();
-	let dd = day.getDate();
-	let mm = day.getMonth();
-
-	let yyyy = day.getFullYear();
-	if(dd<10) {
-		dd = '0' + dd;
-	};
-	if(mm<10) {
-		mm = '0' + mm;
-	}
-	day = mm+'/'+dd+'/'+yyyy;
-	return day;
-}
-
-let today = new Date().toLocaleString("en-US").split(',')[0];
+let clientID;
 
 function getClientInfoFromSpotify() {
 	let settings = {
@@ -126,12 +181,13 @@ function getClientInfoFromSpotify() {
 	$.ajax(settings);
 }
 
-let clientID;
-// getClientInfoFromSpotify();
-
 
 //Create playlist
+let today = new Date().toLocaleString("en-US").split(',')[0];
+
+
 let playlistID;
+
 function createPlaylist(id) {
 	let settings = {
 		async: true,
@@ -177,62 +233,48 @@ function addTracksToPlaylist(array) {
 	$.ajax(settings);
 }
 
-let spotifyArtistsSingleTopTracks = [];
+
 
 //Initiate Spotify app
+let spotifyArtistsSingleTopTracks = [];
+
 function spotifyAppInitiate() {
 	const spotifyArtists = artistList.map(getArtistsFromSpotify);
 	let spotifyArtistsIDs = [];
 	let spotifyArtistsTopTracks = [];
 	Promise.all(spotifyArtists)
 		.then(data => {
-			// console.log(data);
-			spotifyArtistsIDs = data.map(a => a.artists.items[0].id);
-			// console.log(spotifyArtistsIDs);
+			console.log(data);
+			const filteredData = data.filter(a => a.artists.items.length > 0);
+			console.log(filteredData);
+			spotifyArtistsIDs = filteredData.map(a => a.artists.items[0].id);
 			Promise.all(spotifyArtistsIDs.map(getArtistsTracksFromSpotify))
 				.then(data => {
-					spotifyArtistsSingleTopTracks = data.map(a => "spotify:track:" + a.tracks[0].id);
+					console.log(data)
+					const filteredTracks = data.filter(a => a.tracks.length > 0);
+					console.log(filteredTracks);
+					debugger;
+					spotifyArtistsSingleTopTracks = filteredTracks.map(a => "spotify:track:" + a.tracks[0].id);
 					console.log(spotifyArtistsSingleTopTracks)
 					getClientInfoFromSpotify();
 				})
 		})
-
-	// let spotifyArtistsIDs = [];
-	// let spotifyArtistsTopTracks = [];
-	// let spotifyArtistsSingleTopTracks = [];
-
-	// $.when(...spotifyArtists)
-	// 	.then((...spotifyArtists) => {
-	// 		spotifyArtistsIDs = spotifyArtists.map(a => a[0].artists.items[0].id);
-	// 		console.log(spotifyArtists);
-	// 		console.log(spotifyArtistsIDs);
-	// 		$.when(...spotifyArtistsIDs)
-	// 			.then((...spotifyArtistsIDs) => {
-	// 				spotifyArtistsTopTracks = spotifyArtistsIDs.map(getArtistsTracksFromSpotify);
-	// 				console.log(spotifyArtistsTopTracks);
-	// 				$.when(...spotifyArtistsTopTracks)
-	// 					.then((...spotifyArtistsTopTracks) => {
-	// 						spotifyArtistsSingleTopTracks = spotifyArtistsTopTracks.map(b => b[0].tracks[0].id);
-	// 						console.log(spotifyArtistsSingleTopTracks);
-	// 					})
-						
-					
-	// 			})
-	// 	});
-
-	
-		
-
 };
 
+
+//------------------------------------------------------------
+//Putting it all together
+//------------------------------------------------------------
 
 
 function watchSubmit() {
 	$('#search-form').submit(e => {
 		e.preventDefault();
-		displayConcertListings(concertList);
-		createArtistList(concertList);
-		spotifyAppInitiate();
+		const queryTarget = $(e.currentTarget).find('#search-term');
+		const query = queryTarget.val();
+		getMetroAreaIDFromSongKick(query);
+		
+		
 
 
 	})
